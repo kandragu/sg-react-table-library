7b32a63acb40686733cd2d04c84d99eed6193ece
Rahu <rahunandan@gmail.com>
2023-06-02 16:14:32 +0200

    fix:publish issue

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
-export { default as Button } from './Button';
+export { default as Table } from './Table';
 export * from './Button';
+export * from './Table';
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 import './styles/index.scss';
-
 export * from './components';
